20d94b315c13de4d0bbc45db8792bda66689b2d3
Jayesh Savaliya <112215167@cse.iiitp.ac.in>
2024-07-30 10:00:58 +0530

    hello

--- utils/securePassword.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcrypt");
-
-const securePassword = async (password) => {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, 	salt);
-    return {hashedPassword,salt};
-};
-module.exports = securePassword;
